5267ddbda357acd943025aeafbf0afbcb34ea73b
Detective Loki <41474934+abiB27@users.noreply.github.com>
2019-11-06 18:52:41 +0530

    Create download.py
    v1
    -download image
    -input >> url
    -name is randomly assigned
    -folder is predefined

---
+++ download.py
@@ -0,0 +1,25 @@
+'''useful module to download media from internet'''
+import requests
+import os
+from PIL import Image
+import hashlib
+import io
+
+
+def image1(url) :
+    folder_path = path = "C:\\Users\\ABIJITH\\Pictures\\scraped"
+    try:
+        image_content = requests.get(url).content
+
+    except Exception as e:
+        print(f"ERROR - Could not download {url} - {e}")
+
+    try:
+        image_file = io.BytesIO(image_content)
+        image = Image.open(image_file).convert('RGB')
+        file_path = os.path.join(folder_path,hashlib.sha1(image_content).hexdigest()[:10] + '.jpg')
+        with open(file_path, 'wb') as f:
+            image.save(f, "JPEG", quality=85)
+        print(f"SUCCESS - saved {url} - as {file_path}")
+    except Exception as e:
+        print(f"ERROR - Could not save {url} - {e}")
